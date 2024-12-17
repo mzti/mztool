031697e86508a430b539ab8bd47298779df75fad
DANIELMOZARTT <daniel.mozart@icloud.com>
2024-12-17 18:30:03 -0300

    up

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -2140,8 +2140,8 @@ function Pro {
     SLMGR.VBS /CPKY
     SLMGR.VBS /CKMS
     Net stop Sppsvc
-    CD C:\Windows\System32\SPP\Store\2.0
-    Ren Tokens.dat Tokens.old
+    Set-Location C:\Windows\System32\SPP\Store\2.0
+    Rename-Item Tokens.dat Tokens.old
     SLMGR.VBS /RILC
     changepk.exe /ProductKey VK7JG-NPHTM-C97JM-9MPGT-3V66T
 
