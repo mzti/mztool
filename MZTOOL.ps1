33141218f1c09c751cb65f8ac42176a1b497e489
DANIELMOZARTT <daniel.mozart@icloud.com>
2025-02-23 15:30:34 -0300

    up

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -1,3 +1,37 @@
+<#
+.SYNOPSIS
+    Instalação e personalização automatizada de softwares e o perfil de usuário no ambiente Windows.
+
+.DESCRIPTION
+    Instale softwares e personaliza o perfil de usuário no Windows automaticamente a partir da nuvem e de pacotes e módulos da Microsoft. 
+    
+.NOTES
+    Autor: Daniel Mozart - https://www.linkedin.com/in/danielmozart/
+    Compatibilidade: Windows 11 e 10.
+     
+.EXAMPLE
+
+    1 - Implementação automatizada:
+
+Módulos e Gerenciador de pacotes (MSIX, NuGet): Implementação, atualização e auto-reparo do Winget e PSWindowsUpdate.
+
+Softwares (Winget): Adobe Acrobat Reader, Google Chrome, Microsoft 365, Powershell, AnyDesk.
+
+Atualizações (PSWindowsUpdate): Remoção de drivers de dispositivo não utilizados pelo sistema atualmente (Dispositivos Ocultos) e Implementação e Atualização de novos e atuais Drivers de Dispositivo e Atualizações do Windows Update.
+
+Personalização do Perfil de Usuário (Regedit, XML, Appx): Tema, Ícones da Área de Trabalho e Barra de Tarefas, Remoção de Widgets, Remoção de Bloatwares, Remoção do Microsoft Copilot, Remoção de Ícones Visão de Tarefas e Notícias, Remoção de notificações da Central de Ações, Define o Google Chrome como navegador padrão e o Acrobat Reader como leitor de PDF padrão.
+2 - Download e execução standalone automatizada em nuvem de softwares para monitoramento e diagnóstico de hardwares.
+
+HDSentinel, AIDA64, CPUZ, BlueScreenView, Core Temp, Crystal Disk Info, HWInfo, GPUZ.
+3 - Atualização automátizada de softwares e drivers através do Winget e Módulo Windows Update.
+
+4 - Implementação automatizada de diferentes versões do Pacote Office e Microsoft 365.
+
+.LINK
+    https://github.com/DanielMozartt/MZTOOL
+    
+#>
+
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 
 #Obtém o ID e o Objeto de Segurança do usuário atual.
