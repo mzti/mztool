57291745d46ca6b335151cca1025c570c67d1e43
DANIELMOZARTT <daniel.mozart@icloud.com>
2025-04-07 16:48:13 -0300

    up

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -783,7 +783,7 @@ function DownloadMztool {
     #Verifica se o link do OneDrive está disponível, se não estiver, verifica se o link do Google Drive está disponível.
     $MZTOOLZIP = "$Env:TOOL\MZTOOL.zip"
 
-    $ONEDRIVELINK = 'https://bit.ly/MZTZZZIP'
+    $ONEDRIVELINK = 'https://bit.ly/MZTZIP'
        
     $GOOGLEDRIVELINK = 'https://drive.usercontent.google.com/download?id=19eiKJbx55RgkV_KczFrkL7uMkxjVrMo9&confirm=yy'
     function LINKSTATUS {
