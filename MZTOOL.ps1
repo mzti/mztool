b37510e8e258922191c8da105482598c878c200f
DANIELMOZARTT <daniel.mozart@icloud.com>
2025-05-12 23:03:01 -0300

    UP

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -1944,23 +1944,29 @@ function NEWPWSH {
     
     # Converte o conteúdo para Base64 para uso com -EncodedCommand
     $encodedCommand = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($combinedDefinitions))
-    $arguments = @(<#'-noprofile',#> '-EncodedCommand', $encodedCommand)
+    $arguments = @('-EncodedCommand', $encodedCommand)
     
     if ($Wait) {
         # Caso Wait seja especificado, aguardamos o término do processo internamente.
         [void](Start-Process powershell -ArgumentList $arguments -Wait)
     }
-
       
     if ($Hidden) {
         # Caso Wait seja especificado, aguardamos o término do processo internamente.
         [void](Start-Process powershell -ArgumentList $arguments -WindowStyle Hidden)
     }
+    
+    elseif ($Wait -and $Hidden) {
+        # Caso Wait e Hidden sejam especificados, aguardamos o término do processo internamente.
+        [void](Start-Process powershell -ArgumentList $arguments -WindowStyle Hidden -Wait)
+    }
+
     elseif ($ReturnProcess) {
         # Se o usuário quer o objeto do processo para controlar externamente, retornamos-o.
         $proc = Start-Process powershell -ArgumentList $arguments -PassThru
         return $proc
     }
+
     else {
         # Se nada for especificado, usamos a forma que não retorna nada – compatível com [void](...)
         [void](Start-Process powershell -ArgumentList $arguments)
