bcc5b0acaf07563150b4a39fb22461c163e1125c
DANIELMOZARTT <daniel.mozart@icloud.com>
2025-05-27 10:58:49 -0300

    up

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -77,15 +77,13 @@ OPSYS
 
 function PSVER {
     PAUSE
-    #Verifica se a versão do PowerShell é suportada.
-
-    
+    #Verifica se a versão do PowerShell é suportada.    
 
     if ($Global:PSVER -lt [version]$Global:MZPSVER ) {
-        Write-Host "VERSÃO NECESSÁRIA DO POWERSHELL:$($Global:MZPSVER) ou superior"
-        Write-Host "`n`VERSÃO ATUAL DO POWERSHELL: "$($Global:PSVER)""
+        Write-Host "VERSÃO NECESSÁRIA DO POWERSHELL:"$($Global:MZPSVER)" OU SUPERIOR."
+        Write-Host "`n`VERSÃO ATUAL DO POWERSHELL  :"$($Global:PSVER)""
 
-        Write-Host "`n`nATUALIZE A SUA VERSÃO DO POWERSHELL PARA CONTINUAR: `nhtps://github.com/PowerShell/PowerShell/releases/download/v7.5.1/PowerShell-7.5.1-win-x64.msi "
+        Write-Host "`n`nATUALIZE A SUA VERSÃO DO POWERSHELL PARA CONTINUAR: `nhtps://github.com/PowerShell/PowerShell/releases/download/v7.5.1/PowerShell-7.5.1-win-x64.msi" -ForegroundColor Cyan
         
         Read-Host "`n`nPRESSIONE ENTER PARA SAIR" | Out-Null
         PAUSE
@@ -762,11 +760,11 @@ $MYWINDOWSPRINCIPAL = New-Object System.Security.Principal.WindowsPrincipal($MYW
 $ADMINROLE = ([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 
 function RESTARTADMIN {
-    <#$RESTART = New-Object System.Diagnostics.ProcessStartInfo 'PowerShell'
+    $RESTART = New-Object System.Diagnostics.ProcessStartInfo 'PowerShell'
     $RESTART.Arguments = "-Command `"${global:SCRIPTCODE}`""
     $RESTART.Verb = 'runas'
-    [System.Diagnostics.Process]::Start($RESTART) | Out-Null#>
-    Start-Process -FilePath "wt.exe" -ArgumentList "poweshell.exe -ExecutionPolicy Bypass -Command $Global:SCRIPTCODE" -Verb RunAs
+    [System.Diagnostics.Process]::Start($RESTART) | Out-Null
+    #Start-Process -FilePath "wt.exe" -ArgumentList "poweshell.exe -ExecutionPolicy Bypass -Command $Global:SCRIPTCODE" -Verb RunAs
     EXIT
 }
 
