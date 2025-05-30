ca01e0cee08f96272810377c6fe09664eb6a6f03
DANIELMOZARTT <daniel.mozart@icloud.com>
2025-05-30 11:54:37 -0300

    up

--- MODULES/MZTOOL.psm1
+++ MODULES/MZTOOL.psm1
@@ -3,7 +3,7 @@
 #region Variáveis Globais
 $Global:TITLE = "MZTOOL BETA"
 $Global:DESKTOP = "C:\Users\Public\DESKTOP"
-$Global:MZTOOLMODULE = Get-Module -Name "MZTOOL" -ErrorAction SilentlyContinue
+$Global:MZTOOLMODULE = { Get-Module -Name "MZTOOL" -ErrorAction SilentlyContinue }
 $Global:EXECUTIONPOLICY = { Get-ExecutionPolicy -List -ErrorAction SilentlyContinue }
 $Global:WINVER = (Get-CimInstance Win32_OperatingSystem).Caption, (Get-CimInstance -Class Win32_OperatingSystem).OSArchitecture
 #endregion
