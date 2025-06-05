e70406ef83121103bd81df1a8d0db3c7a2371b89
DANIELMOZARTT <daniel.mozart@icloud.com>
2025-06-05 13:18:01 -0300

    up

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -38,7 +38,7 @@ HDSentinel, AIDA64, CPUZ, BlueScreenView, Core Temp, Crystal Disk Info, HWInfo, 
 
 Clear-Host
 
-param([int]$Restarted)
+param([swicth]$Restarted)
 if ($Restarted) { Write-host "$Restarted" }
 Pause
 
