fc373bb12bae3e7296a0c6a67093ed4c8969df78
DANIELMOZARTT <daniel.mozart@icloud.com>
2025-07-03 20:09:17 -0300

    up

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -2423,7 +2423,7 @@ function MICROSOFT365 {
     
     #Verifica se o Microsoft 365 já está instalado.
     #$MS365 = { Get-Command "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE" -ErrorAction SilentlyContinue }
-    $MS365 = { $null }
+    $MS365 = $null 
     #$INSTALLED = UNINSTALLOFFICE
 
     $INSTALLED = $null
