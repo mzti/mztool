1b0240aabca5aa97c6ff84b003a2dc4f7381ee7e
DANIELMOZARTT <daniel.mozart@icloud.com>
2025-07-08 20:24:53 -0300

    up

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -2425,9 +2425,9 @@ function MICROSOFT365 {
     
     #Verifica se o Microsoft 365 já está instalado.
     $MS365 = { Get-Command "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE" -ErrorAction SilentlyContinue }
-    pause  
+      
     $INSTALLED = UNINSTALLOFFICE    
-    pause   
+       
     if (-not ($INSTALLED)) {             
         
         #Cria o arquivo XML de instalação personalizada no diretório %TEMP%.
@@ -2452,7 +2452,7 @@ function MICROSOFT365 {
   <RemoveMSI />
 </Configuration> 
 '@        
-        pause
+        
         function POSTINSTALLM365 {
 
             if (& $MS365) {
@@ -2516,8 +2516,7 @@ function MICROSOFT365 {
         }
 
         else {
-            $M365STATUS = 2  
-            pause          
+            $M365STATUS = 2                        
         }    
 
     }
@@ -2525,7 +2524,7 @@ function MICROSOFT365 {
     else {        
         
         $M365STATUS = 3
-        pause
+        
     }   
 
     return $M365STATUS
