ffdd840629a845de1572f96701293a46f10413fd
DANIELMOZARTT <daniel.mozart@icloud.com>
2026-03-02 15:03:31 -0300

    UP

--- TERRAFORM/UPLOADFILE/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cloudfront_distribution_id" {
-  type        = string
-  description = "ID da distribuição CloudFront"
-}
-
-variable "mztool_zip" {
-  type        = string
-  description = "MZTOOL.ZIP"
-}
