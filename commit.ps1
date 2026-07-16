5465f75013eb61b51b3f816d38aea54613027c42
DANIELMOZARTT <daniel.mozart@icloud.com>
2026-07-16 11:53:03 -0300

    Updating files

--- commit.ps1
+++ commit.ps1
@@ -1,3 +1,6 @@
+$cert = Get-ChildItem Cert:\CurrentUser\Root\DD9FFF3030DCDCD38E28A7AD6CB3E69AF3FDE848
+Set-AuthenticodeSignature -FilePath .\mztool.ps1 -Certificate $cert
+
 git add .
 git commit -m "Updating files"
 git push
