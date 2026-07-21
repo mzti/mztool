31b438807db16631b3f797f0674372f662e27546
DANIELMOZARTT <daniel.mozart@icloud.com>
2026-07-21 18:14:13 -0300

    Testing mztool.ps1 certificate and github actions api on terraform.

---
+++ terraform/uploadfile/main.tf
@@ -0,0 +1,37 @@
+resource "aws_s3_object" "mztool_zip" {
+  bucket = "mztool"
+  key    = "MZTOOL.zip"
+  source = "${var.mztool_zip}"
+  etag   = filesha256("${var.mztool_zip}")
+}
+
+output "mztool_zip_sha256" {
+  value = filesha256("${var.mztool_zip}")
+}
+
+resource "null_resource" "trigger_mztool_sign_workflow" {
+  # depende do upload do ZIP
+  depends_on = [
+    aws_s3_object.mztool_zip
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOT
+curl -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${var.github_pat} \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/${var.github_repo}/actions/workflows/mztool-sign.yml/dispatches \
+  -d '{"ref":"main"}'
+EOT
+  }
+}
+
+resource "null_resource" "invalidate_cloudfront" {
+  depends_on = [aws_s3_object.mztool_zip]
+
+provisioner "local-exec" {
+  command = "aws cloudfront create-invalidation --distribution-id ${var.cloudfront_distribution_id} --paths /MZTOOL.zip"
+}
+
+}
---
+++ terraform/uploadfile/variables.tf
@@ -0,0 +1,19 @@
+variable "cloudfront_distribution_id" {
+  type        = string
+  description = "ID da distribuição CloudFront"
+}
+
+variable "mztool_zip" {
+  type        = string
+  description = "MZTOOL.ZIP"
+}
+
+variable "github_pat" {
+  type      = string
+  sensitive = true
+}
+
+variable "github_repo" {
+  type = string
+  # exemplo: "mzti/mztool"
+}
