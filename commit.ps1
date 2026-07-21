4ea7ca72ccbc80ff2ff576a186c4477fe4e99ab0
DANIELMOZARTT <daniel.mozart@icloud.com>
2026-07-21 16:26:00 -0300

    Testing mztool.ps1 certificate.

--- commit.ps1
+++
@@ -1,6 +0,0 @@
-$cert = Get-ChildItem Cert:\CurrentUser\Root\DD9FFF3030DCDCD38E28A7AD6CB3E69AF3FDE848
-Set-AuthenticodeSignature -FilePath .\mztool.ps1 -Certificate $cert
-
-git add .
-git commit -m "Updating files"
-git push
